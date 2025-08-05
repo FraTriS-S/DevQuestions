e8ed55f09eebc9b9f7f391e3f93d47cd1a66bd36
Zelyanyakas Denis <brotok2001@yandex.ru>
2025-08-05 21:29:20 +0700

    done with domain models

---
+++ DevQuestions/src/DevQuestions.Domain/Questions/Answer.cs
@@ -0,0 +1,10 @@
+namespace DevQuestions.Domain.Questions;
+
+public class Answer
+{
+    public Guid Id { get; init; }
+    public required string Text { get; init; }
+    public required Guid UserId { get; set; }
+    public List<Guid> Comments { get; set; } = [];
+    public required Question Question { get; set; }
+}
---
+++ DevQuestions/src/DevQuestions.Domain/Reports/Status.cs
@@ -0,0 +1,9 @@
+namespace DevQuestions.Domain.Reports;
+
+public enum Status
+{
+    Open = 1,
+    InProgress = 2,
+    Resolved = 3,
+    Dismissed = 4,
+}
